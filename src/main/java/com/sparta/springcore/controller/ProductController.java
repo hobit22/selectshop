dfde77d2d5f8657c1537733fee3b6eddfcdabf14
hobit22 <hobit22@naver.com>
2022-03-26 22:40:07 +0900

    Refactoring

--- src/main/java/com/sparta/springcore/controller/ProductController.java
+++ src/main/java/com/sparta/springcore/controller/ProductController.java
@@ -1,15 +1,21 @@
-package com.sparta.springcore;
+package com.sparta.springcore.controller;
+
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.dto.ProductMypriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
 
-//@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
 
     private final ProductService productService;
 
+    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
@@ -19,7 +25,7 @@ public class ProductController {
     public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
         Product product = productService.createProduct(requestDto);
 
-        // 응답 보내기
+// 응답 보내기
         return product;
     }
 
@@ -28,7 +34,7 @@ public class ProductController {
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
         Product product = productService.updateProduct(id, requestDto);
 
-        // 응답 보내기 (업데이트된 상품 id)
+// 응답 보내기 (업데이트된 상품 id)
         return product.getId();
     }
 
@@ -37,7 +43,7 @@ public class ProductController {
     public List<Product> getProducts() throws SQLException {
         List<Product> products = productService.getProducts();
 
-        // 응답 보내기
+// 응답 보내기
         return products;
     }
 }
