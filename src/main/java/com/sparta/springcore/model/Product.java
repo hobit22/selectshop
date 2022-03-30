c011cd42816674caf345f4d0f85383a4d04aafb7
hobit22 <hobit22@naver.com>
2022-03-30 18:50:05 +0900

    3-5 * 테스트 코드 추가 ProductValidator URLValidator

--- src/main/java/com/sparta/springcore/model/Product.java
+++ src/main/java/com/sparta/springcore/model/Product.java
@@ -1,17 +1,21 @@
 package com.sparta.springcore.model;
+
 import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.validator.ProductValidator;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 @Setter
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
 @Entity // DB 테이블 역할을 합니다.
 public class Product {
-
     // ID가 자동으로 생성 및 증가합니다.
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
@@ -38,7 +42,10 @@ public class Product {
 
     // 관심 상품 생성 시 이용합니다.
     public Product(ProductRequestDto requestDto, Long userId) {
-// 관심상품을 등록한 회원 Id 저장
+        // 입력값 Validator
+        ProductValidator.validateProductInput(requestDto, userId);
+
+        // 관심상품을 등록한 회원 Id 저장
         this.userId = userId;
         this.title = requestDto.getTitle();
         this.image = requestDto.getImage();
@@ -46,4 +53,5 @@ public class Product {
         this.lprice = requestDto.getLprice();
         this.myprice = 0;
     }
+
 }
