689225e81a046447d3c7fc00ac6ad36120c2adb0
hobit22 <hobit22@naver.com>
2022-04-05 03:08:23 +0900

    4-9 * 페이지네이션 추가

--- src/test/java/com/sparta/springcore/integration/ProductIntegrationTest.java
+++ src/test/java/com/sparta/springcore/integration/ProductIntegrationTest.java
@@ -1,5 +1,4 @@
 package com.sparta.springcore.integration;
-
 import com.sparta.springcore.dto.ProductMypriceRequestDto;
 import com.sparta.springcore.dto.ProductRequestDto;
 import com.sparta.springcore.model.Product;
@@ -7,18 +6,15 @@ import com.sparta.springcore.service.ProductService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ProductIntegrationTest {
-    //통합테스트라 Autowired 사용 가능 Bean 사용 가능
     @Autowired
     ProductService productService;
 
@@ -30,7 +26,7 @@ class ProductIntegrationTest {
     @Order(1)
     @DisplayName("신규 관심상품 등록")
     void test1() {
-        // given
+// given
         String title = "Apple <b>에어팟</b> 2세대 유선충전 모델 (MV7N2KH/A)";
         String imageUrl = "https://shopping-phinf.pstatic.net/main_1862208/18622086330.20200831140839.jpg";
         String linkUrl = "https://search.shopping.naver.com/gate.nhn?id=18622086330";
@@ -42,10 +38,10 @@ class ProductIntegrationTest {
                 lPrice
         );
 
-        // when
+// when
         Product product = productService.createProduct(requestDto, userId);
 
-        // then
+// then
         assertNotNull(product.getId());
         assertEquals(userId, product.getUserId());
         assertEquals(title, product.getTitle());
@@ -53,8 +49,6 @@ class ProductIntegrationTest {
         assertEquals(linkUrl, product.getLink());
         assertEquals(lPrice, product.getLprice());
         assertEquals(0, product.getMyprice());
-
-        // Order 2 에서 사용하기 위해 createdProduct 생성해서 넘겨줌
         createdProduct = product;
     }
 
@@ -62,15 +56,15 @@ class ProductIntegrationTest {
     @Order(2)
     @DisplayName("신규 등록된 관심상품의 희망 최저가 변경")
     void test2() {
-        // given
+// given
         Long productId = this.createdProduct.getId();
         int myPrice = 70000;
         ProductMypriceRequestDto requestDto = new ProductMypriceRequestDto(myPrice);
 
-        // when
+// when
         Product product = productService.updateProduct(productId, requestDto);
 
-        // then
+// then
         assertNotNull(product.getId());
         assertEquals(userId, product.getUserId());
         assertEquals(this.createdProduct.getTitle(), product.getTitle());
@@ -85,20 +79,24 @@ class ProductIntegrationTest {
     @Order(3)
     @DisplayName("회원이 등록한 모든 관심상품 조회")
     void test3() {
-        // given
+// given
+        int page = 0;
+        int size = 10;
+        String sortBy = "id";
+        boolean isAsc = false;
 
-        // when
-        List<Product> productList = productService.getProducts(userId);
+// when
+        Page<Product> productList = productService.getProducts(userId, page, size, sortBy, isAsc);
 
-        // then
-        // 1. 전체 상품에서 테스트에 의해 생성된 상품 찾아오기 (상품의 id 로 찾음)
+// then
+// 1. 전체 상품에서 테스트에 의해 생성된 상품 찾아오기 (상품의 id 로 찾음)
         Long createdProductId = this.createdProduct.getId();
         Product foundProduct = productList.stream()
                 .filter(product -> product.getId().equals(createdProductId))
                 .findFirst()
                 .orElse(null);
 
-        // 2. Order(1) 테스트에 의해 생성된 상품과 일치하는지 검증
+// 2. Order(1) 테스트에 의해 생성된 상품과 일치하는지 검증
         assertNotNull(foundProduct);
         assertEquals(userId, foundProduct.getUserId());
         assertEquals(this.createdProduct.getId(), foundProduct.getId());
@@ -107,7 +105,7 @@ class ProductIntegrationTest {
         assertEquals(this.createdProduct.getLink(), foundProduct.getLink());
         assertEquals(this.createdProduct.getLprice(), foundProduct.getLprice());
 
-        // 3. Order(2) 테스트에 의해 myPrice 가격이 정상적으로 업데이트되었는지 검증
+// 3. Order(2) 테스트에 의해 myPrice 가격이 정상적으로 업데이트되었는지 검증
         assertEquals(this.updatedMyPrice, foundProduct.getMyprice());
     }
 }
